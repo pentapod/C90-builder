596d1e80057908db2c2b8b3ee7adcd49b3a05010
spring-raining <fdonthavei@gmail.com>
2016-07-09 03:09:44 +0900

    Update gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,13 +7,18 @@ import pug from 'pug';
 
 const $ = gulpLoadPlugins();
 
+require('prismjs/plugins/line-numbers/prism-line-numbers');
+
 pug.filters.code = function(str, options, locals) {
   const opts = Object.assign({}, options || {}, locals || {});
   const lang = opts.lang || 'plain';
+  const start = (typeof opts.start === 'number')? opts.start : null;
   const highlighted = (Prism.languages[opts.lang])
     ? Prism.highlight(str, Prism.languages[opts.lang])
     : str;
-  return `<pre><code class="language-${lang}">${highlighted}</code></pre>`;
+  return `<pre class="${(start !== null)? 'line-numbers' : ''}" ${(start !== null)? 'data-start="'+start+'"' : ''}>`
+       +   `<code class="language-${lang}">${highlighted}</code>`
+       + `</pre>`;
 }
 
 gulp.task('default', ['pug']);
@@ -21,8 +26,13 @@ gulp.task('default', ['pug']);
 gulp.task('pug', () =>
   gulp.src('content/index.pug')
     .pipe($.pug({
-    //  pug: pug,
+      pug: pug,
       pretty: true,
     }))
-    .pipe(gulp.dest('content/dest/'))
+    .pipe(gulp.dest('dest/'))
 );
+
+gulp.task('watch', () =>
+  gulp.watch('content/**/*.pug', ['pug'])
+);
+
